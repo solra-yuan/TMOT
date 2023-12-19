3f845f83ef7c9a5769e0b5fa4e490988ce22fa79
W.S.Son <thsdnjstjq1@naver.com>
2023-12-19 17:46:29 +0900

    Fix

--- src/solar_yuan_types.py
+++ src/solar_yuan_types.py
@@ -1,15 +1,33 @@
-from typing import TypedDict, Any, Tuple, NotRequired
+from typing import TypedDict, Any, Tuple, NotRequired, TypeAlias
 from torch import Tensor
 from PIL import Image
 
-from pycocotools import coco
+TPolygonSegmentation: TypeAlias = list[list[float]]
+
+# Unused in this module, but imported in multiple submodules.
+class EncodedRLE(TypedDict):  # noqa: Y049
+    size: list[int]
+    counts: str | bytes
+
+class RLE(TypedDict):
+    size: list[int]
+    counts: list[int]
+
+class Annotation(TypedDict):
+    id: int
+    image_id: int
+    category_id: int
+    segmentation: TPolygonSegmentation | RLE | EncodedRLE
+    area: float
+    bbox: list[float]
+    iscrowd: int
 
 class RandomState(TypedDict):
     random: Tuple[Any, ...]
     torch: Tensor
 
-class CocoAnnotationExtensions(coco._Annotation):
+class CocoAnnotationExtensions(Annotation):
     prev_image: NotRequired[Image.Image]
-    prev_target: NotRequired[coco._Annotation]
+    prev_target: NotRequired[Annotation]
     prev_prev_image: NotRequired[Image.Image]
-    prev_prev_target: NotRequired[coco._Annotation]
+    prev_prev_target: NotRequired[Annotation]
