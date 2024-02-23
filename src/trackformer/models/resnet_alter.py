12e93bd9c053cdbdb7184a0feb9a461237456ebb
W.S.Son <thsdnjstjq1@naver.com>
2024-02-23 13:55:03 +0900

    Thermal rgb cleanup (#24)
    * Code cleanup
    
    * Code cleanup
    
    * temporary fix

--- src/trackformer/models/resnet_alter.py
+++ src/trackformer/models/resnet_alter.py
@@ -24,7 +24,8 @@ model_urls = {
 
 
 class ResNet4Channel(ResNet):
-    def __init__(self,
+    def __init__(
+            self,
         block: Type[Union[BasicBlock, Bottleneck]],
         layers: List[int],
         num_classes: int = 1000,
@@ -34,81 +35,37 @@ class ResNet4Channel(ResNet):
         replace_stride_with_dilation: Optional[List[bool]] = None,
         norm_layer: Optional[Callable[..., nn.Module]] = None
     ) -> None:
-        super().__init__(block, 
-        layers,
-        num_classes,
-        zero_init_residual,
-        groups,
-        width_per_group,
-        replace_stride_with_dilation,
-        norm_layer)
+        super().__init__(
+            block,
+            layers,
+            num_classes,
+            zero_init_residual,
+            groups,
+            width_per_group,
+            replace_stride_with_dilation,
+            norm_layer
+        )
 
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        self._norm_layer = norm_layer
-
-        self.inplanes = 64
-        self.dilation = 1
-        if replace_stride_with_dilation is None:
-            # each element in the tuple indicates if we should replace
-            # the 2x2 stride with a dilated convolution instead
-            replace_stride_with_dilation = [False, False, False]
-        if len(replace_stride_with_dilation) != 3:
-            raise ValueError("replace_stride_with_dilation should be None "
-                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
-
-        self.conv_rgbt_to_latent = nn.Conv2d(4, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv_rgbt_to_latent = nn.Conv2d(
+            4, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
         self.rgbt_bn = nn.BatchNorm2d(self.inplanes)
         self.rgbt_relu = nn.ReLU(inplace=True)
         self.rgbt_maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         rgbt_downsample = nn.Sequential(
-                conv1x1(self.inplanes, 256, stride=1),
-                norm_layer(256))
-        self.preprocess_latent_channel = nn.Sequential(
-            Bottleneck(self.inplanes, 64, 
-                    stride=1, 
-                    downsample=rgbt_downsample, 
-                    groups=1, 
-                    base_width=64, 
-                    dilation=1)
+            conv1x1(self.inplanes, 256, stride=1),
+            norm_layer(256)
         )
-        self.conv_inplane_to_rgb = nn.Conv2d(self.inplanes*4, 3, kernel_size=3, stride=1, padding=1, bias=False)
+        self.preprocess_latent_channel = nn.Sequential(
+            Bottleneck(self.inplanes, 64,
+                       stride=1,
+                       downsample=rgbt_downsample,
+                       groups=1,
+                       base_width=64,
+                       dilation=1)
+        )
+        self.conv_inplane_to_rgb = nn.Conv2d(
+            self.inplanes*4, 3, kernel_size=3, stride=1, padding=1, bias=False)
 
-        
-        self.groups = groups
-        self.base_width = width_per_group
-        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
-                                       dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
-                                       dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
-                                       dilate=replace_stride_with_dilation[2])
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
-        if zero_init_residual:
-            for m in self.modules():
-                if isinstance(m, Bottleneck):
-                    nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
-                elif isinstance(m, BasicBlock):
-                    nn.init.constant_(m.bn2.weight, 0)
-        
     def forward(self, x: Tensor) -> Tensor:
         x = self.conv_rgbt_to_latent(x)
         x = self.rgbt_bn(x)
@@ -118,7 +75,8 @@ class ResNet4Channel(ResNet):
         x = self.conv_inplane_to_rgb(x)
 
         return super().forward(x)
-    
+
+
 def _resnet_4_channel(
     arch: str,
     block: Type[Union[BasicBlock, Bottleneck]],
@@ -134,6 +92,7 @@ def _resnet_4_channel(
         model.load_state_dict(state_dict, strict=False)
     return model
 
+
 def resnet50_4_channel(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> ResNet:
     r"""ResNet-50 model from
     `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_.
@@ -142,7 +101,13 @@ def resnet50_4_channel(pretrained: bool = False, progress: bool = True, **kwargs
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    pretrained_resnet = _resnet_4_channel('resnet50', Bottleneck, [3, 4, 6, 3], pretrained, progress,
-                   **kwargs)
-    
+    pretrained_resnet = _resnet_4_channel(
+        'resnet50',
+        Bottleneck,
+        [3, 4, 6, 3],
+        pretrained,
+        progress,
+        **kwargs
+    )
+
     return pretrained_resnet
