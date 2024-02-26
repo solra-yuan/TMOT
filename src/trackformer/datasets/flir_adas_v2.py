ecd6497a6c76176051cb1f82040326cb1b83e3eb
insookim43 <insookim43@gmail.com>
2024-02-26 21:24:56 +0900

    fix: flir_adas_v2_concat inherits CocoDetection

--- src/trackformer/datasets/flir_adas_v2.py
+++ src/trackformer/datasets/flir_adas_v2.py
@@ -264,15 +264,51 @@ class FLIR_ADAS_V2_thermal(CocoDetection):
                     writer.writerow(d)
 
 
-class FLIR_ADAS_V2_concat(Dataset):
-    # concat dataset은 두 개의 데이터셋을 받아서
-    def __init__(self, dataset1: FLIR_ADAS_V2, dataset2: FLIR_ADAS_V2_thermal):
-        self.dataset1 = dataset1
-        self.dataset2 = dataset2
+class FLIR_ADAS_V2_concat(CocoDetection):
+    def __init__(self, 
+                 *args, 
+                 dataset_rgb: FLIR_ADAS_V2, 
+                 dataset_thermal: FLIR_ADAS_V2_thermal, 
+                 prev_frame_range=1, 
+                 random_state_dict=None, 
+                 flir_concat_size_tuple=None, 
+                 **kwargs
+                ):
+        super(FLIR_ADAS_V2_concat, self).__init__(*args, **kwargs)
+
+        self._prev_frame_range = prev_frame_range
+        if random_state_dict is not None:
+            self.random_state_dict = random_state_dict
+        else:
+            self.random_state_dict = None
+        if flir_concat_size_tuple:
+            self.flir_concat_size_tuple = flir_concat_size_tuple
+        else:
+            self.flir_concat_size_tuple = None
+
+        self.dataset_rgb = dataset_rgb
+        self.dataset_thermal = dataset_thermal
+
+    @property
+    def sequences(self):
+        return self.coco.dataset['sequences']
+
+    @property
+    def frame_range(self):
+        if 'frame_range' in self.coco.dataset:
+            return self.coco.dataset['frame_range']
+        else:
+            return {'start': 0, 'end': 1.0}
+    
+    def seq_length(self, idx):
+        return self.coco.imgs[idx]['seq_length']
+
+    def sample_weight(self, idx):
+        return 1.0 / self.seq_length(idx)
 
     def __len__(self):
-        l1 = len(self.dataset1)
-        l2 = len(self.dataset2)
+        l1 = len(self.dataset_rgb)
+        l2 = len(self.dataset_thermal)
         assert l1 == l2,  'given concat element datasets length is different.'
 
         return l1
@@ -281,22 +317,35 @@ class FLIR_ADAS_V2_concat(Dataset):
 
     def __getitem__(self, idx):
         # image를 받아서 concat해서 돌려줌 # target?
-        img1, target1 = self.dataset1[idx]
-        img2, target2 = self.dataset2[idx]
+        img1, target1 = self.dataset_rgb[idx]
+        img2, target2 = self.dataset_thermal[idx]
         
         # shared_transform = T.Resize([512, 640])
 
         # img1 = shared_transform(img1)
         # img2 = shared_transform(img2)
         concatenated_img = torch.cat([img1, img2[0:1,:,:]], dim=0)
-        if target1.get('prev_image') is not None and target2.get('prev_image') is not None:
-            concatenated_prev_img = torch.cat([target1['prev_image'], target2['prev_image'][0:1,:,:]], dim=0)
-        
-        target = target1
-        
-        target['prev_image'] = concatenated_prev_img
-        # concatenated_target = torch.cat([target1, target2])
 
+        target = target1
+
+        if target1.get('prev_image') is not None and target2.get('prev_image') is not None:
+            concatenated_prev_img = torch.cat([
+                target1['prev_image'], 
+                target2['prev_image'][0:1,:,:]], 
+                dim=0
+            )
+            target['prev_image'] = concatenated_prev_img
+
+
+        if target1.get('prev_prev_image') is not None and target2.get('prev_prev_image') is not None:
+            concatenated_prev_prev_img = torch.cat([
+                target1['prev_prev_image'], 
+                target2['prev_prev_image'][0:1,:,:]], 
+                dim=0
+            )
+            target['prev_prev_image'] = concatenated_prev_prev_img
+
+        
         # return concatenated_img, target1, target2
         return concatenated_img, target
 
@@ -429,8 +478,7 @@ class WeightedConcatDataset(torch.utils.data.ConcatDataset):
 
 
 def build_flir_adas_v2(image_set, args):
-    # 이녀석의 역할
-    # image set에 따른 root 지정
+    # Set dataset training phase: {train, val}
     if image_set == 'train':
         root = Path(args.flir_adas_v2_path_train)
         prev_frame_rnd_augs = args.track_prev_frame_rnd_augs
@@ -467,7 +515,7 @@ def build_flir_adas_v2(image_set, args):
 
 
 def build_flir_adas_v2_concat(image_set, args):
-    # 데이터셋의 훈련 페이즈 지정
+    # Set dataset training phase: {train, val}
     if image_set == 'train':
         root = Path(args.flir_adas_v2_path_train)
         prev_frame_rnd_augs = args.track_prev_frame_rnd_augs
@@ -480,44 +528,24 @@ def build_flir_adas_v2_concat(image_set, args):
         ValueError(f'unknown {image_set}')
 
     assert root.exists(), f'provided flir_adas_v2 Det path {root} does not exist'
-    # read split attribute
-    split = getattr(args, f"{image_set}_split")  # train_split: 'train_coco'
-    # img_folder와 annotation 찾기
-    img_folder = root / split  #
-    ann_file = root / f"annotations/{split}.json"  # train_coco.json
-    # argument에 따른 transform 지정
+    
+
+    # transform
     transforms, norm_transforms = make_coco_transforms(
         image_set, args.img_transform, args.overflow_boxes)  # transform before dataset
     flir_concat_random_state_dict = {
                 'random': random.getstate(),
                 'torch': torch.random.get_rng_state()}
     flir_rgb_t_concat_size_tuple = (640, 512)
-    
-    # FLIR_ADAS_V2 데이터셋 만들기
-    dataset1 = FLIR_ADAS_V2(
-        img_folder, ann_file, transforms, norm_transforms,
-        prev_frame_range=prev_frame_range,
-        random_state_dict = flir_concat_random_state_dict,
-        flir_concat_size_tuple = flir_rgb_t_concat_size_tuple,
-        return_masks=args.masks,
-        overflow_boxes=args.overflow_boxes,
-        remove_no_obj_imgs=False,
-        prev_frame=args.tracking,
-        prev_frame_rnd_augs=prev_frame_rnd_augs,
-        prev_prev_frame=args.track_prev_prev_frame,
-    )
-    # read split attribute : thermal
-    # 'train_coco_t'
-    split = getattr(args, f"flir_adas_v2_thermal_{image_set}_split")
-    # img folder와 annotation 찾기
+
+
+    # flir_adas_v2_thermal
+    split = getattr(args, f"flir_adas_v2_thermal_{image_set}_split")  # 'train_coco_t'
     img_folder = root / split
     ann_file = root / f"annotations/{split}.json"  # 'train_coco_t.json'
-    # argument에 따른 transform 지정
     # transform 어떻게 줘야 하지? 일단 img_transform max_size:1333 val_width 800이긴한데..
 
-    # FLIR_ADAS_V2 : thermal 데이터셋 만들기
-    # thermal dataset은 따로 생성함, arguments를 어떻게 줘야하지?
-    dataset2 = FLIR_ADAS_V2_thermal(
+    dataset_thermal = FLIR_ADAS_V2_thermal(
         img_folder, ann_file, transforms, norm_transforms,
         prev_frame_range=prev_frame_range,
         random_state_dict = flir_concat_random_state_dict,
@@ -529,7 +557,39 @@ def build_flir_adas_v2_concat(image_set, args):
         prev_frame_rnd_augs=prev_frame_rnd_augs,
         prev_prev_frame=args.track_prev_prev_frame,
     )
+
+    # flir_adas_v2
+    split = getattr(args, f"{image_set}_split")  # train_split: 'train_coco'
+    img_folder = root / split  #
+    ann_file = root / f"annotations/{split}.json"  # train_coco.json
+
+
+    dataset_rgb = FLIR_ADAS_V2(
+        img_folder, ann_file, transforms, norm_transforms,
+        prev_frame_range=prev_frame_range,
+        random_state_dict = flir_concat_random_state_dict,
+        flir_concat_size_tuple = flir_rgb_t_concat_size_tuple,
+        return_masks=args.masks,
+        overflow_boxes=args.overflow_boxes,
+        remove_no_obj_imgs=False,
+        prev_frame=args.tracking,
+        prev_frame_rnd_augs=prev_frame_rnd_augs,
+        prev_prev_frame=args.track_prev_prev_frame,
+    )
+
     # concat each dataset class
-    dataset = FLIR_ADAS_V2_concat(dataset1, dataset2)
+    dataset = FLIR_ADAS_V2_concat(
+        img_folder, ann_file, transforms, norm_transforms,
+        dataset_rgb=dataset_rgb, 
+        dataset_thermal=dataset_thermal,
+        prev_frame_range=prev_frame_range,
+        random_state_dict = flir_concat_random_state_dict,
+        flir_concat_size_tuple = flir_rgb_t_concat_size_tuple,
+        return_masks=args.masks,
+        overflow_boxes=args.overflow_boxes,
+        remove_no_obj_imgs=False,
+        prev_frame=args.tracking,
+        prev_frame_rnd_augs=prev_frame_rnd_augs,
+        prev_prev_frame=args.track_prev_prev_frame)
 
     return dataset
